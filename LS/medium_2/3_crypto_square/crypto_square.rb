7f90b18c9a5ed1f6b46b787b890a6d08bb010880
Paul Kuttiya <w.kuttiya@gmail.com>
2017-07-23 23:12:49 -0400

    cypher text

---
+++ LS/medium_2/3_crypto_square/crypto_square.rb
@@ -0,0 +1,65 @@
+# remove space and punctuation, and downcase
+# break input in rows with new line
+# rows, cols create with perfect square
+# cipher message
+
+#logic
+#use regex to remove get clean output
+#find length for output
+#find the sq root of the length then round up = n
+#output message in array, with n chunk of letters for each row 
+#re-arrange array element into new array, by each element index 
+
+class Crypto
+  def initialize(message)
+    @message = message.downcase.gsub(/\W/, '')
+  end
+
+  def normalize_plaintext
+    @message
+  end
+
+  def size
+    Math.sqrt(normalize_plaintext.length).ceil
+  end
+
+  def plaintext_segments
+    # result = []
+    # text = normalize_plaintext.chars
+
+    # size.times do 
+    #   result << text.shift(size).join('') if !text.empty?
+    # end
+    
+    # result
+
+    normalize_plaintext.scan(/.{1,#{size}}/) #concise way with .scan ruby API
+  end
+
+  def ciphertext
+    cipher_join
+  end
+
+  def normalize_ciphertext
+    cipher_join(' ')
+  end
+
+  private
+  def cipherhash
+    result = {}
+
+    plaintext_segments.each do |chunk|
+      chunk.chars.each_with_index do |letter, idx|
+        text = result[idx]
+        result[idx] =  result[idx] || letter
+        result[idx] += letter if !text.nil?
+      end
+    end
+
+    result
+  end
+
+  def cipher_join(joiner='')
+    cipherhash.map { |_, v| v}.join(joiner)    
+  end
+end
